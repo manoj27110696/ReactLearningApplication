c0d9664eacf2d4192892bcbad01087b6e7705209
Sindhu Choppara <sindhu.choppara@gmail.com>
2022-04-06 01:09:32 -0400

    testing toggle

--- react-learn/src/App.tsx
+++ react-learn/src/App.tsx
@@ -1,32 +1,24 @@
-import React, {useState, useEffect} from 'react';
+import React, {useState} from 'react';
 import './App.css';
 
 
 
 
-
 function App() {
-  const [emotion,setEmotion]= useState("happy");
-  const[secondary,setSecondary]= useState("1")
-  useEffect(()=> {
-    console.log(`Its ${emotion} around here`);
-  },[emotion]);
+  const[checkedValue,setChecked]= useState(true);
+  function toggle() {
+  setChecked((checkedValue: any) => !checkedValue);
+}
 
-  useEffect(()=> {
-    console.log(`Secondary is ${secondary}`);
-  },[secondary]);
-  
-  
+ 
   return (
     <>
-    <h1>Current emotion is {emotion} and {secondary}</h1>
-    <button onClick={() => setEmotion("checking")}>
-      Checking
-    </button>
-    <button onClick={() => setEmotion("happy")}>  Happy</button>
-    <button onClick={() => setSecondary("2")}>Secondary</button>
-    <button onClick={() => setEmotion("sad")}>Sad</button>
-    <button onClick={() => setEmotion("angry")}>Angry</button>
+    <input type="checkbox" 
+           checked={checkedValue}
+           onChange={toggle}
+    />
+     <p>{checkedValue ? "checked" :"not checked"}</p>
+ 
     </>
   );
 }
