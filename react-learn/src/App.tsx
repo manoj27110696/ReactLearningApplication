fa4770c3a9b84418d41baeaae1cd3c7da0dd5033
Sindhu Choppara <sindhu.choppara@gmail.com>
2022-04-07 00:28:52 -0400

    loading and fetching data and handling State

--- react-learn/src/App.tsx
+++ react-learn/src/App.tsx
@@ -1,26 +1,44 @@
-import React, {useState} from 'react';
+/* eslint-disable import/no-extraneous-dependencies */
+import React, { useState, useEffect } from 'react';
+import PropTypes from 'prop-types';
 import './App.css';
+import { Profile } from './Interfaces/GithubProfile';
 
+//https://api.github.com/users/manoj27110696
 
+function App({ login }: { login: string }) {
+	const [data, setData] = useState<Profile>();
+	const [loading, setLoading] = useState<boolean>(false);
+	const [error, setError] = useState(null);
 
+	useEffect(() => {
+		if (!login) return;
+		setLoading(true);
+		fetch(`https://api.github.com/users/${login}`)
+			.then((response) => response.json())
+			.then(setData)
+			.then(() => setLoading(false))
+			.catch(setError);
+	}, [login]);
 
-function App() {
-  const[checkedValue,setChecked]= useState(true);
-  function toggle() {
-  setChecked((checkedValue: any) => !checkedValue);
+	if (loading) return <h1>Loading..</h1>;
+	if (error) return <pre>{JSON.stringify(error, null, 2)}</pre>;
+  if(!data) return null;
+
+	if (data) {
+		return (
+			<div>
+				<h1>Name:{data.name}</h1>
+				<p>Location: {data.location}</p>
+				<img alt={data.login} src={data.avatar_url} />
+			</div>
+		);
+	}
+	return <div>No data</div>;
 }
 
- 
-  return (
-    <>
-    <input type="checkbox" 
-           checked={checkedValue}
-           onChange={toggle}
-    />
-     <p>{checkedValue ? "checked" :"not checked"}</p>
- 
-    </>
-  );
-}
+App.propTypes = {
+	login: PropTypes.string.isRequired,
+};
 
 export default App;
--- react-learn/src/index.tsx
+++ react-learn/src/index.tsx
@@ -8,7 +8,7 @@ import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <App login="manoj2711069"/>
   </React.StrictMode>,
 
 
